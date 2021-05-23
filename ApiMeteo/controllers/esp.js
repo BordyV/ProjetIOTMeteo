ca3223bb032ed4d8ae70a5460114f8027476662a
yohann <yohann.laurendeau@etu.unice.fr>
2021-05-23 18:52:56 +0200

    Ajout requete addEsp

--- ApiMeteo/controllers/esp.js
+++ ApiMeteo/controllers/esp.js
@@ -13,7 +13,22 @@ const getAll = async (req,res) => {
             res.send({message: error.message})
         })
 }
-
+const newEsp= async (req,res) => {
+    const data = req.body;
+    const newEsp = new EspModel({
+        adresseMac : data.adresseMac,
+        adresse : data.adresse,
+        userId: data.userId,
+    });
+    await newEsp.save()
+    .then(data => {
+        res.json({message: "Sucess, correctly added to the dataBASE"});
+    })
+    .catch(err => {
+        res.status(400).json({message : err.message});
+    })
+}
 module.exports = {
     getAll : getAll,
+    newEsp : newEsp,
 }
--- ApiMeteo/routes/esp.js
+++ ApiMeteo/routes/esp.js
@@ -1,7 +1,7 @@
 const espController = require('../controllers/esp');
 const express = require('express');
 const router = express.Router();
-
+router.post('/addEsp', espController.newEsp); // créer un nouvel utilisateur (voir le model pour le body)
 
 router.get('/', espController.getAll);
 
