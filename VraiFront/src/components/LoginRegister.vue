e7e6fcef1ec521b64380fe314141f525cbdc5090
Valentin <valentin.bordy@hotmail.com>
2021-05-28 18:12:52 +0200

    refractor: message erreur formulaire login

--- VraiFront/src/components/LoginRegister.vue
+++ VraiFront/src/components/LoginRegister.vue
@@ -107,7 +107,7 @@ export default {
     },
     computed: {
     passwordMatch() {
-      return () => this.password === this.verify || "Password must match";
+      return () => this.password === this.verify || "Le mot de passe n'est pas le meme.";
     }
   },
   methods: {
@@ -196,7 +196,7 @@ export default {
             // 
         }else{
             this.loginPassword = '';
-            alert('Wrong credentials');
+            alert('Mauvais identifiants.');
         }
     })
   },
@@ -227,18 +227,18 @@ export default {
     loginPassword: "",
     loginEmail: "",
     loginEmailRules: [
-      v => !!v || "Required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      v => !!v || "Requis.",
+      v => /.+@.+\..+/.test(v) || "l'E-mail doit être valide"
     ],
     emailRules: [
-      v => !!v || "Required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      v => !!v || "Requis.",
+      v => /.+@.+\..+/.test(v) || "l'E-mail doit être valide"
     ],
 
     show1: false,
     rules: {
-      required: value => !!value || "Required.",
-      min: v => (v && v.length >= 8) || "Min 8 characters"
+      required: value => !!value || "Requis.",
+      min: v => (v && v.length >= 8) || "Minimum de 8 caractères"
     }
 })
 }
