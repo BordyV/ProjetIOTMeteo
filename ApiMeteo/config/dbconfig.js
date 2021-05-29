05e7a21235877232d0b96d085d735cc69a24d637
Valentin <valentin.bordy@hotmail.com>
2021-05-29 02:20:13 +0200

    doc: dbconfig.js
    ajout de ligne de commentaire pour le fichier dbconfing.js

--- ApiMeteo/config/dbconfig.js
+++ ApiMeteo/config/dbconfig.js
@@ -1,3 +1,5 @@
+//environnement de dev
 const uri = 'mongodb+srv://iot:root@cluster0.efowh.mongodb.net/METEO?retryWrites=true&w=majority';
+//environnement de prod 
 //const uri = 'mongodb://localhost:27017/METEO';
 module.exports = uri;
