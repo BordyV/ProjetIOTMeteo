d2da8e7a680062e316520675a93413f2caab1a6c
Valentin <valentin.bordy@hotmail.com>
2021-05-31 13:30:25 +0200

    Update serv2.js

--- ApiMeteo/serv2.js
+++ ApiMeteo/serv2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const https = require('https');
 const mongoose = require('mongoose');
 const DB_URI = require('./config/dbconfig');
