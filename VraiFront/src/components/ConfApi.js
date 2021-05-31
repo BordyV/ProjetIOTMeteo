cd01b555a05c551666d1e211994d2b4818ac8e86
Valentin <valentin.bordy@hotmail.com>
2021-05-31 13:15:26 +0200

    refactor: change uri

--- VraiFront/src/components/ConfApi.js
+++ VraiFront/src/components/ConfApi.js
@@ -1,10 +1,10 @@
 
 //environnement de dev
-const uriApi = 'localhost:3000';
+//const uriApi = 'localhost:3000';
 
 //environnement de prod 
 //const uriApi = 'https://valentinbordy.fr/meteo';
 
 //2nd environnement de prod (heroku)
-//const uriApi = 'https://menez-tp4-bordy.herokuapp.com';
+const uriApi = 'https://iotmeteobordy.herokuapp.com/';
 module.exports = uriApi;
